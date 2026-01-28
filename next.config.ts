ff3554434bf8233cd881151253cd85c4825cba03
Elena Sinelnikova <elle.sinelle@gmail.com>
2026-01-28 12:10:05 +0300

    fix: update next.config for vercel webpack deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     poweredByHeader: false,
+
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+        config.resolve.alias['@/ui'] = path.resolve(__dirname, 'src/components/ui')
+
+        return config
+    },
 }
 
 export default nextConfig
